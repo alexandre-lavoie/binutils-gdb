9a81efb5b5994364552d048d348ba1dde5e6e2f9
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2023-10-29 02:34:25 -0400

    [DCPU16] Assemble without link

--- gas/config/tc-dcpu16.c
+++ gas/config/tc-dcpu16.c
@@ -1,92 +1,410 @@
 #include "as.h"
 #include "bfd.h"
+#include "opcode/dcpu16.h"
 
 const char *md_shortopts = "";
 struct option md_longopts[] = {};
 size_t md_longopts_size = sizeof(md_longopts);
 
-// TODO: Comment chars and seperators
 const char comment_chars[] = "";
-const char line_comment_chars[] = "//";
-const char line_separator_chars[] = ";";
+const char line_comment_chars[] = ";";
+const char line_separator_chars[] = "";
 
-// No floating point
 const char EXP_CHARS[] = "";
 const char FLT_CHARS[] = "";
 
-// TODO: Pseudo ops
-const pseudo_typeS md_pseudo_table[] =
-{
-  { (char*)0, (void(*)(int))0, 0 }
-};
+const pseudo_typeS md_pseudo_table[] = {{ (char*)0, (void(*)(int))0, 0 }};
 
-int md_parse_option(int c ATTRIBUTE_UNUSED, const char *arg ATTRIBUTE_UNUSED)
+void
+md_show_usage(FILE *stream ATTRIBUTE_UNUSED)
 {
-  // TODO: Parse
+  fprintf(stream, "\nDCPU16 options: None\n");
+}
+
+int
+md_parse_option(int c ATTRIBUTE_UNUSED, const char *arg ATTRIBUTE_UNUSED)
+{
   return 0;
 }
 
-void md_show_usage(FILE *stream ATTRIBUTE_UNUSED)
+static htab_t dcpu16_op_hsh;
+static htab_t dcpu16_value_hsh;
+
+void
+md_begin(void)
 {
-  // TODO: CLI usage
-  fprintf(stream, "\nDCPU16 options: none available yet\n");
+  dcpu16_op_hsh = str_htab_create();
+  know(dcpu16_op_hsh != NULL);
+
+  for (size_t i = 0; i < dcpu16_op_map_size; i++)
+  {
+    const dcpu16_op_entry *entry = dcpu16_op_map + i;
+    const char *name = entry->name;
+
+    str_hash_insert(dcpu16_op_hsh, name, (void*)entry, 0);
+  }
+
+  dcpu16_value_hsh = str_htab_create();
+  know(dcpu16_value_hsh != NULL);
+
+  for (size_t i = 0; i < dcpu16_value_map_size; i++)
+  {
+    const dcpu16_value_entry *entry = dcpu16_value_map + i;
+    const char *name = entry->name;
+
+    symbolS *sym = symbol_new(name, reg_section, &zero_address_frag, i);
+    know(sym != NULL);
+
+    str_hash_insert(dcpu16_value_hsh, name, (void*)sym, 0);
+  }
 }
 
-void md_begin(void)
+static bool
+parse_opcode(char **str, const dcpu16_op_entry **op)
 {
-  // TODO: Initializer hook
+  char *ptr = *str;
+
+  *op = str_hash_find_n(dcpu16_op_hsh, ptr, 3);
+  if (*op == NULL) 
+  {
+    as_bad(_("Opcode not found"));
+    return false;
+  }
+
+  *str = ptr + 4;
+  return true;
+}
+
+static bool
+parse_register(char **str, expressionS *exp) 
+{
+  char *ptr = *str;
+  while (*ptr >= 'A' && *ptr <= 'Z') ptr++;
+  if (ptr <= *str) return false;
+
+  symbolS *sym = str_hash_find_n(dcpu16_value_hsh, *str, ptr - *str);
+  if (sym == NULL) return false;
+
+  exp->X_op = O_register;
+  exp->X_add_number = S_GET_VALUE(sym);
+
+  *str = ptr;
+  return true;
+}
+
+static bool
+parse_immediate(char **str, expressionS *exp)
+{
+  char *ptr = *str;
+
+  char radix;
+  if (strncmp(ptr, "0x", 2) == 0) radix = 16;
+  else if (strncmp(ptr, "0b", 2) == 0) radix = 2;
+  else radix = 10;
+
+  char *end;
+  uint16_t result = strtol(ptr, &end, radix);
+
+  if (ptr == end) return false;
+
+  exp->X_op = O_big;
+  exp->X_add_number = result;
+
+  *str = end;
+  return true;
+}
+
+#define is_valid_symbol_char(v) (v >= 'A' && v <= 'Z') || (v >= 'a' && v <= 'z') || (v >= '0' && v <= '9') || v == '_'
+
+static bool
+parse_symbol(char **str, expressionS *exp)
+{
+  char *ptr = *str;
+
+  if (*ptr >= '0' && *ptr <= '9') return false;
+  while (is_valid_symbol_char(*ptr)) ptr++;
+
+  if (ptr == *str) return false;
+  size_t size = ptr - *str;
+
+  char buffer[DCPU16_SYMBOL_BYTE_SIZE] = { 0 };
+  strncpy(buffer, *str, (size < DCPU16_SYMBOL_BYTE_SIZE) ? size : DCPU16_SYMBOL_BYTE_SIZE);
+
+  symbolS *sym = symbol_find_or_make(buffer);
+  know(sym != NULL);
+
+  exp->X_op = O_symbol;
+  exp->X_add_symbol = sym;
+
+  *str = ptr;
+  return true;
+}
+
+static bool
+parse_argument_types(char **str, dcpu16_parse_argument *out)
+{
+  char *ptr = *str;
+
+  if (parse_register(&ptr, &out->reg));
+  else if (parse_immediate(&ptr, &out->imm));
+  else if (parse_symbol(&ptr, &out->imm));
+  else 
+  {
+    as_bad(_("Invalid argument type"));
+    return false;
+  }
+
+  *str = ptr;
+  return true;
+}
+
+static bool
+parse_argument(char **str, dcpu16_parse_argument *out) 
+{
+  char *ptr = *str;
+
+  bool is_value_of = *ptr == '[';
+  if (is_value_of) 
+  {
+    ptr++;
+
+    out->reg.X_md |= DCPU16_VALUE_OF_MASK;
+    out->imm.X_md |= DCPU16_VALUE_OF_MASK;
+  }
+
+  if (!parse_argument_types(&ptr, out)) return false;
+
+  if (*ptr == '+') 
+  {
+    ptr++;
+
+    bool with_reg = out->reg.X_op != 0;
+
+    if (!parse_argument_types(&ptr, out)) return false;
+
+    if (
+      (with_reg && out->imm.X_op == 0)
+      || (!with_reg && out->reg.X_op == 0)
+    )
+    {
+      as_bad(_("Invalid add argument type"));
+      return false;
+    }
+  }
+
+  if (is_value_of && *(ptr++) != ']')
+  {
+    as_bad(_("Missing closing bracket"));
+    return false;
+  }
+
+  *str = ptr;
+  return true;
+}
+
+static bool
+parse_instruction(char **str, dcpu16_parse_instruction *out)
+{
+  char *ptr = *str;
+
+  if (!parse_opcode(&ptr, &out->op)) return false;
+
+  if (!parse_argument(&ptr, &out->lhs)) return false;
+
+  bool is_double_arg = out->op->bop != BOP_SOP;
+  if (is_double_arg) {
+    if (*(ptr++) != ',') 
+    {
+      as_bad(_("Expected comma"));
+      return false;
+    }
+
+    if (!parse_argument(&ptr, &out->rhs)) return false;
+  }
+
+  if (*ptr != ';' && *ptr != '\0')
+  {
+    as_bad(_("Expected end of line"));
+    return false;
+  }
+
+  *str = ptr;
+  return true;
+}
+
+static inline void
+optimize_small_immediate(const dcpu16_parse_argument *in, dcpu16_build_argument *out)
+{
+  if (in->imm.X_op == O_symbol) return;
+
+  if (out->imm == UINT16_MAX) {
+    out->val = VAL_LIT;
+    out->has_imm = false;
+  } else if (out->imm <= 0x1E) {
+    out->val = VAL_LIT + (uint8_t)out->imm + 1;
+    out->has_imm = false;
+  }
+}
+
+static bool
+is_valid_immediate(const dcpu16_parse_argument *in, dcpu16_build_argument *out)
+{
+  if (in->reg.X_md != 0 && in->imm.X_md != 0 && out->val > VAL_REG_J)
+  {
+    as_bad(_("Immediate only allowed for base registers"));
+    return false;
+  }
+
+  return true;
+}
+
+static void
+resolve_immediate(const dcpu16_parse_argument *in, dcpu16_build_argument *out)
+{
+  out->has_imm = in->imm.X_op != 0;
+  if (in->imm.X_op == O_symbol) out->imm = symbol_get_value_expression(in->imm.X_add_symbol)->X_add_number;
+  else if (in->imm.X_op == O_big) out->imm = in->imm.X_add_number;
+}
+
+static void
+handle_immediate(const dcpu16_parse_argument *in, dcpu16_build_argument *out)
+{
+  bool vof = (in->reg.X_md & DCPU16_VALUE_OF_MASK) > 0;
+  
+  bool with_reg = in->reg.X_op != 0;
+  bool with_imm = out->has_imm;
+
+  if (with_reg && vof && out->val <= VAL_REG_J) out->val += with_imm ? VAL_AT_IMM_REG : VAL_AT_REG;
+
+  if (!with_reg && with_imm) out->val = vof ? VAL_AT_IMM : VAL_IMM;
+}
+
+static bool
+build_argument(const dcpu16_parse_argument *in, dcpu16_build_argument *out)
+{
+  if (in->reg.X_op != 0) out->val = dcpu16_value_map[in->reg.X_add_number].value;
+
+  resolve_immediate(in, out);
+  if (!is_valid_immediate(in, out)) return false;
+  handle_immediate(in, out);
+
+  return true;
+}
+
+static bool
+build_instruction(const dcpu16_parse_instruction *in, dcpu16_build_instruction *out)
+{
+  const dcpu16_parse_argument *a_in;
+  if (in->op->bop == BOP_SOP)
+  {
+    if (!build_argument(&in->lhs, &out->a)) return false;
+    out->b.val = in->op->sop;
+    a_in = &in->lhs;
+  }
+  else
+  {
+    if (!build_argument(&in->lhs, &out->b)) return false;
+    if (!build_argument(&in->rhs, &out->a)) return false;
+    a_in = &in->rhs;
+  }
+
+  optimize_small_immediate(a_in, &out->a);
+
+  out->op = in->op->bop;
+
+  return true;
+}
+
+static inline void
+emit_opcode(const dcpu16_build_instruction *in)
+{
+  uint16_t insn = 
+    ((in->a.val & 0b111111) << 0xA) | 
+    ((in->b.val & 0b011111) << 0x5) |
+    ((in->op    & 0b011111) << 0x0) ;
+
+  char *frag = frag_more(DCPU16_INSTRUCTION_BYTE_SIZE);
+  md_number_to_chars(frag, insn, DCPU16_INSTRUCTION_BYTE_SIZE);
+}
+
+static inline void
+emit_immediate(const dcpu16_build_argument *in)
+{
+  if (!in->has_imm) return;
+
+  char *frag = frag_more(DCPU16_IMMEDIATE_BYTE_SIZE);
+  md_number_to_chars(frag, in->imm, DCPU16_INSTRUCTION_BYTE_SIZE);
+}
+
+static void
+emit_instruction(const dcpu16_build_instruction *in)
+{
+  emit_opcode(in);
+
+  emit_immediate(&in->b);
+  emit_immediate(&in->a);
+}
+
+void
+md_assemble(char *str)
+{
+  dcpu16_parse_instruction p = (dcpu16_parse_instruction) { 0 };
+  if (!parse_instruction(&str, &p)) return;
+
+  dcpu16_build_instruction b = (dcpu16_build_instruction) { 0 };
+  if (!build_instruction(&p, &b)) return;
+
+  emit_instruction(&b);
+}
+
+symbolS
+*md_undefined_symbol(char *name ATTRIBUTE_UNUSED)
+{
+  return 0;
+}
+
+const char
+*md_atof(int type ATTRIBUTE_UNUSED, char *lit ATTRIBUTE_UNUSED, int *size ATTRIBUTE_UNUSED)
+{
+  return 0;
+}
+
+valueT
+md_section_align(asection *seg ATTRIBUTE_UNUSED, valueT size ATTRIBUTE_UNUSED)
+{
+  int align = bfd_section_alignment(seg);
+  int new_size = ((size + (1 << align) -1) & -(1 << align));
+
+  return new_size;
+}
+
+void
+md_convert_frag(bfd *abfd ATTRIBUTE_UNUSED, asection *seg ATTRIBUTE_UNUSED, fragS *fragp ATTRIBUTE_UNUSED)
+{
   return;
 }
 
-void md_assemble(char *insn_str ATTRIBUTE_UNUSED)
+void
+md_apply_fix(fixS *fixp ATTRIBUTE_UNUSED, valueT *val ATTRIBUTE_UNUSED, segT seg ATTRIBUTE_UNUSED)
 {
-  // TODO: Main assembler hook
   return;
 }
 
-symbolS *md_undefined_symbol(char *name ATTRIBUTE_UNUSED)
+arelent
+*tc_gen_reloc(asection *seg ATTRIBUTE_UNUSED, fixS *fixp ATTRIBUTE_UNUSED)
 {
-  // TODO: 'md_parse_name' fails
   return 0;
 }
 
-const char *md_atof(int type ATTRIBUTE_UNUSED, char *lit ATTRIBUTE_UNUSED, int *size ATTRIBUTE_UNUSED)
-{
-  // Not used
-  return 0;
-}
-
-valueT md_section_align(asection *seg ATTRIBUTE_UNUSED, valueT val ATTRIBUTE_UNUSED)
-{
-  // TODO: Round up section
-  return 0;
-}
-
-void md_convert_frag(bfd *abfd ATTRIBUTE_UNUSED, asection *seg ATTRIBUTE_UNUSED, fragS *fragp ATTRIBUTE_UNUSED)
-{
-  // Not used
-  return;
-}
-
-void md_apply_fix(fixS *fixp ATTRIBUTE_UNUSED, valueT *val ATTRIBUTE_UNUSED, segT seg ATTRIBUTE_UNUSED)
-{
-  // Not used
-  return;
-}
-
-arelent *tc_gen_reloc(asection *seg ATTRIBUTE_UNUSED, fixS *fixp ATTRIBUTE_UNUSED)
-{
-  // Not used
-  return 0;
-}
-
-long md_pcrel_from(fixS *fixp ATTRIBUTE_UNUSED)
+long
+md_pcrel_from(fixS *fixp ATTRIBUTE_UNUSED)
 {
   as_fatal(_("unexpected call"));
   return 0;
 }
 
-int md_estimate_size_before_relax(fragS *fragp ATTRIBUTE_UNUSED, asection *seg ATTRIBUTE_UNUSED)
+int
+md_estimate_size_before_relax(fragS *fragp ATTRIBUTE_UNUSED, asection *seg ATTRIBUTE_UNUSED)
 {
   as_fatal(_("unexpected call"));
   return 0;
---
+++ include/opcode/dcpu16.h
@@ -0,0 +1,190 @@
+#ifndef DCPU16_H
+#define DCPU16_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DCPU16_SYMBOL_BYTE_SIZE 1024
+
+#define DCPU16_INSTRUCTION_BYTE_SIZE 4
+#define DCPU16_IMMEDIATE_BYTE_SIZE 4
+
+#define DCPU16_VALUE_OF_MASK 0b0000001
+
+enum dcpu16_value
+{
+  VAL_REG_A       = 0x00,
+  VAL_REG_B       = 0x01,
+  VAL_REG_C       = 0x02,
+  VAL_REG_X       = 0x03,
+  VAL_REG_Y       = 0x04,
+  VAL_REG_Z       = 0x05,
+  VAL_REG_I       = 0x06,
+  VAL_REG_J       = 0x07,
+  VAL_AT_REG      = 0x08 /* to 0x0F */, // [VAL_REG_*]
+  VAL_AT_IMM_REG  = 0x10 /* to 0x17 */, // [VAL_REG_* + next word]
+  VAL_PUSH_POP    = 0x18, // (PUSH / [--SP]) if in b, or (POP / [SP++]) if in a
+  VAL_PEEK        = 0x19, // [SP] / PEEK
+  VAL_PICK        = 0x1A, // [SP + next word] / PICK n
+  VAL_SP          = 0x1B,
+  VAL_PC          = 0x1C,
+  VAL_EX          = 0x1D,
+  VAL_AT_IMM      = 0x1E, // [next word]
+  VAL_IMM         = 0x1F, // next word (literal)
+  VAL_LIT         = 0x20 /* to 0x3F */, // literal value 0xffff-0x1e (-1..30) (only for a)
+};
+
+typedef struct 
+{
+  const char *name;
+  uint8_t value;
+} dcpu16_value_entry;
+
+const dcpu16_value_entry dcpu16_value_map[] = 
+{
+  { "A",      VAL_REG_A },
+  { "B",      VAL_REG_B },
+  { "C",      VAL_REG_C },
+  { "X",      VAL_REG_X },
+  { "Y",      VAL_REG_Y },
+  { "Z",      VAL_REG_Y },
+  { "I",      VAL_REG_I },
+  { "J",      VAL_REG_J },
+  { "SP",     VAL_SP },
+  { "PUSH",   VAL_PUSH_POP },
+  { "POP",    VAL_PUSH_POP },
+  { "PEEK",   VAL_PEEK },
+  { "PC",     VAL_PC },
+  { "EX",     VAL_EX }
+};
+const size_t dcpu16_value_map_size = sizeof(dcpu16_value_map) / sizeof(dcpu16_value_map[0]);
+
+enum dcpu16_basic_opcode
+{
+  BOP_SOP = 0x00, // Special Instruction
+  BOP_SET = 0x01,
+  BOP_ADD = 0x02,
+  BOP_SUB = 0x03,
+  BOP_MUL = 0x04,
+  BOP_MLI = 0x05,
+  BOP_DIV = 0x06,
+  BOP_DVI = 0x07,
+  BOP_MOD = 0x08,
+  BOP_MDI = 0x09,
+  BOP_AND = 0x0A,
+  BOP_BOR = 0x0B,
+  BOP_XOR = 0x0C,
+  BOP_SHR = 0x0D,
+  BOP_ASR = 0x0E,
+  BOP_SHL = 0x0F,
+  BOP_IFB = 0x10,
+  BOP_IFC = 0x11,
+  BOP_IFE = 0x12,
+  BOP_IFN = 0x13,
+  BOP_IFG = 0x14,
+  BOP_IFA = 0x15,
+  BOP_IFL = 0x16,
+  BOP_IFU = 0x17,
+  BOP_ADX = 0x1A,
+  BOP_SBX = 0x1B,
+  BOP_STI = 0x1E,
+  BOP_STD = 0x1F,
+};
+
+enum dcpu16_special_opcode
+{
+  SOP_NIL = 0x00,
+  SOP_JSR = 0x01,
+  SOP_INT = 0x08,
+  SOP_IAG = 0x09,
+  SOP_IAS = 0x0A,
+  SOP_RFI = 0x0B,
+  SOP_IAQ = 0x0C,
+  SOP_HWN = 0x10,
+  SOP_HWQ = 0x11,
+  SOP_HWI = 0x12
+};
+
+typedef struct
+{
+  const char *name;
+  uint8_t bop;
+  uint8_t sop; 
+} dcpu16_op_entry;
+
+const dcpu16_op_entry dcpu16_op_map[] = 
+{
+  { "set", BOP_SET, SOP_NIL },
+  { "add", BOP_ADD, SOP_NIL },
+  { "sub", BOP_SUB, SOP_NIL },
+  { "mul", BOP_MUL, SOP_NIL },
+  { "div", BOP_DIV, SOP_NIL },
+  { "dvi", BOP_DVI, SOP_NIL },
+  { "mod", BOP_MOD, SOP_NIL },
+  { "mdi", BOP_MDI, SOP_NIL },
+  { "and", BOP_AND, SOP_NIL },
+  { "bor", BOP_BOR, SOP_NIL },
+  { "xor", BOP_XOR, SOP_NIL },
+  { "shr", BOP_SHR, SOP_NIL },
+  { "asr", BOP_ASR, SOP_NIL },
+  { "shl", BOP_SHL, SOP_NIL },
+  { "ifb", BOP_IFB, SOP_NIL },
+  { "ifc", BOP_IFC, SOP_NIL },
+  { "ife", BOP_IFE, SOP_NIL },
+  { "ifn", BOP_IFN, SOP_NIL },
+  { "ifg", BOP_IFG, SOP_NIL },
+  { "ifa", BOP_IFA, SOP_NIL },
+  { "ifl", BOP_IFL, SOP_NIL },
+  { "ifu", BOP_IFU, SOP_NIL },
+  { "adx", BOP_ADX, SOP_NIL },
+  { "sbx", BOP_SBX, SOP_NIL },
+  { "sti", BOP_STI, SOP_NIL },
+  { "std", BOP_STD, SOP_NIL },
+
+  { "jsr", BOP_SOP, SOP_JSR },
+  { "int", BOP_SOP, SOP_INT },
+  { "iag", BOP_SOP, SOP_IAG },
+  { "ias", BOP_SOP, SOP_IAS },
+  { "rfi", BOP_SOP, SOP_RFI },
+  { "iaq", BOP_SOP, SOP_IAQ },
+  { "hwn", BOP_SOP, SOP_HWN },
+  { "hwq", BOP_SOP, SOP_HWQ },
+  { "hwi", BOP_SOP, SOP_HWI },
+};
+const size_t dcpu16_op_map_size = sizeof(dcpu16_op_map) / sizeof(dcpu16_op_map[0]);
+
+typedef struct
+{
+  expressionS reg;
+  expressionS imm;
+} dcpu16_parse_argument;
+
+typedef struct
+{
+  const dcpu16_op_entry *op;
+  dcpu16_parse_argument lhs;
+  dcpu16_parse_argument rhs;
+} dcpu16_parse_instruction;
+
+typedef struct
+{
+  uint8_t val;
+  bool has_imm;
+  uint16_t imm;
+} dcpu16_build_argument;
+
+typedef struct
+{
+  uint8_t op;
+  dcpu16_build_argument b;
+  dcpu16_build_argument a;
+} dcpu16_build_instruction;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DCPU16_H */
